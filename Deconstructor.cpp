cdf88c730db3fe1fe7ee27275f1eb0fcfb625773
Jintae Lee <37683734+TAEKnical@users.noreply.github.com>
2018-07-17 17:20:11 +0900

    18.07.17(Day2)

---
+++ Deconstructor.cpp
@@ -0,0 +1,35 @@
+// 소멸자 호출 확인하기
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class Test
+{
+	char c;
+
+public:
+	Test(char _c)
+	{
+		c = _c;
+		cout << "생성자 호출 " << c << endl;
+	}
+
+	~Test()
+	{
+		cout << "소멸자 호출 " << c << endl;
+	}
+
+};
+
+void simple_function()
+{
+	Test b('b');
+}
+
+int main()
+{
+	Test a('a');
+	simple_function();
+}
